86f0e6e2217c2f0ff379712bd68a57522d7522b3
James <james.bidule@gmail.com>
2025-04-28 20:17:56 +0200

    chore: enhance error handling on vm module

--- src/modules/vms/application/use-cases/delete-vm.use-case.ts
+++ src/modules/vms/application/use-cases/delete-vm.use-case.ts
@@ -1,9 +1,5 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { VmRepositoryInterface } from '../../domain/interfaces/vm.repository.interface';
-import {
-  VmNotFoundException,
-  VmDeletionException,
-} from '../../domain/exceptions/vm.exception';
 
 @Injectable()
 export class DeleteVmUseCase {
@@ -13,16 +9,7 @@ export class DeleteVmUseCase {
   ) {}
 
   async execute(id: string): Promise<void> {
-    try {
-      const vm = await this.repo.findVmById(id);
-      if (!vm) throw new VmNotFoundException(id);
-
-      await this.repo.deleteVm(id);
-    } catch (error) {
-      if (error instanceof VmNotFoundException) {
-        throw error;
-      }
-      throw new VmDeletionException();
-    }
+    await this.repo.findVmById(id);
+    await this.repo.deleteVm(id);
   }
 }
--- src/modules/vms/application/use-cases/update-vm.use-case.ts
+++ src/modules/vms/application/use-cases/update-vm.use-case.ts
@@ -3,10 +3,6 @@ import { VmRepositoryInterface } from '../../domain/interfaces/vm.repository.int
 import { VmUpdateDto } from '../dto/vm.update.dto';
 import { VmDomainService } from '../../vm.domain.service';
 import { VmResponseDto } from '../dto/vm.response.dto';
-import {
-  VmNotFoundException,
-  VmUpdateException,
-} from '../../domain/exceptions/vm.exception';
 
 @Injectable()
 export class UpdateVmUseCase {
@@ -17,18 +13,9 @@ export class UpdateVmUseCase {
   ) {}
 
   async execute(id: string, dto: VmUpdateDto): Promise<VmResponseDto> {
-    try {
-      const existingVm = await this.repo.findVmById(id);
-      if (!existingVm) throw new VmNotFoundException(id);
-
-      const updatedEntity = this.domain.updateVmEntity(existingVm, dto);
-      const savedVm = await this.repo.save(updatedEntity);
-      return new VmResponseDto(savedVm);
-    } catch (error) {
-      if (error instanceof VmNotFoundException) {
-        throw error;
-      }
-      throw new VmUpdateException();
-    }
+    const existingVm = await this.repo.findVmById(id);
+    const updatedEntity = this.domain.updateVmEntity(existingVm, dto);
+    const savedVm = await this.repo.save(updatedEntity);
+    return new VmResponseDto(savedVm);
   }
 }
