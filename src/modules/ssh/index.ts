38fa9ba06c539e6e5db17add3d215746da28c39f
James ABIB <61329786+jabibamman@users.noreply.github.com>
2025-06-26 23:17:24 +0200

    feat/ssh: websocket ssh gateway

---
+++ src/modules/ssh/index.ts
@@ -0,0 +1,4 @@
+export * from './ssh.module';
+export * from './application/gateway/ssh.gateway';
+export * from './application/use-cases/open-ssh-session.use-case';
+export * from './domain/services/ssh.service';
