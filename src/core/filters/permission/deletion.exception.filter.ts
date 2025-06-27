abf93c8ff44b9a2cd5ec744fac76cc20dd028a5a
James ABIB <61329786+jabibamman@users.noreply.github.com>
2025-06-27 20:15:26 +0200

    feat(clean-filters): refactor permission filters

--- src/core/filters/permission/deletion.exception.filter.ts
+++ src/core/filters/permission/deletion.exception.filter.ts
@@ -1,13 +1,8 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
 import { PermissionDeletionException } from '@/modules/permissions/domain/exceptions/permission.exception';
+import { createSimpleMessageFilter } from '../simple-message.filter';
 
-@Catch(PermissionDeletionException)
-export class PermissionDeletionExceptionFilter implements ExceptionFilter {
-  catch(exception: PermissionDeletionException, host: ArgumentsHost) {
-    const response = host.switchToHttp().getResponse();
-    response.status(500).json({
-      statusCode: 500,
-      message: exception.message,
-    });
-  }
-}
+export const PermissionDeletionExceptionFilter = createSimpleMessageFilter(
+  PermissionDeletionException,
+  HttpStatus.INTERNAL_SERVER_ERROR,
+);
