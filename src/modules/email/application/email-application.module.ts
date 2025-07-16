9c66c1a6b00e91e8aaf7a1013aab332025423116
James ABIB <james.bidule@gmail.com>
2025-07-16 22:09:31 +0200

    chore: move email archi

--- src/modules/email/application/email-application.module.ts
+++ src/modules/email/application/email-application.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SendAccountCreatedEmailUseCase } from './use-cases/send-account-created-email.use-case';
 import { SendResetPasswordEmailUseCase } from './use-cases/send-reset-password-email.use-case';
 import { SendPasswordChangedEmailUseCase } from './use-cases/send-password-changed-email.use-case';
-import { EmailInfrastructureModule } from '../infrastructures/email-infrastructure.module';
+import { EmailInfrastructureModule } from '../infrastructure/email-infrastructure.module';
 import { MAIL_SERVICE_TOKEN } from '../domain/constants/injection-tokens';
 import { ZohoMailAdapter } from '../adapters/zoho-mail.adapter';
 
