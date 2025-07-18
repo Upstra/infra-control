d7468994e084ade5c47823c62d416220e614102b
James ABIB <james.bidule@gmail.com>
2025-07-18 02:13:50 +0200

    feat(email): add UPS battery alert use case to module

--- src/modules/email/application/email-application.module.ts
+++ src/modules/email/application/email-application.module.ts
@@ -3,6 +3,7 @@ import { SendAccountCreatedEmailUseCase } from './use-cases/send-account-created
 import { SendResetPasswordEmailUseCase } from './use-cases/send-reset-password-email.use-case';
 import { SendPasswordChangedEmailUseCase } from './use-cases/send-password-changed-email.use-case';
 import { SendVmwareSyncReportEmailUseCase } from './use-cases/send-vmware-sync-report-email.use-case';
+import { SendUpsBatteryAlertEmailUseCase } from './use-cases/send-ups-battery-alert-email.use-case';
 import { EmailInfrastructureModule } from '../infrastructure/email-infrastructure.module';
 import { MAIL_SERVICE_TOKEN } from '../domain/constants/injection-tokens';
 import { ZohoMailAdapter } from '../adapters/zoho-mail.adapter';
@@ -18,12 +19,14 @@ import { ZohoMailAdapter } from '../adapters/zoho-mail.adapter';
     SendResetPasswordEmailUseCase,
     SendPasswordChangedEmailUseCase,
     SendVmwareSyncReportEmailUseCase,
+    SendUpsBatteryAlertEmailUseCase,
   ],
   exports: [
     SendAccountCreatedEmailUseCase,
     SendResetPasswordEmailUseCase,
     SendPasswordChangedEmailUseCase,
     SendVmwareSyncReportEmailUseCase,
+    SendUpsBatteryAlertEmailUseCase,
   ],
 })
 export class EmailApplicationModule {}
